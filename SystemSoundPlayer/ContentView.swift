7a07e1cffd2d140e49045a3f690e0143e31fc224
Stewart Lynch <slynch@createchsol.com>
2023-07-31 17:13:45 -0700

    Initial Commit

---
+++ SystemSoundPlayer/ContentView.swift
@@ -0,0 +1,28 @@
+//
+// Created for SystemSoundPlayer
+// by  Stewart Lynch on 2023-07-31
+// Using Swift 5.0
+// Running on macOS 14.0
+// 
+// Folllow me on Mastodon: @StewartLynch@iosdev.space
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SystemSoundPlayer/SystemSoundPlayerApp.swift
@@ -0,0 +1,21 @@
+//
+// Created for SystemSoundPlayer
+// by  Stewart Lynch on 2023-07-31
+// Using Swift 5.0
+// Running on macOS 14.0
+// 
+// Folllow me on Mastodon: @StewartLynch@iosdev.space
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+@main
+struct SystemSoundPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
